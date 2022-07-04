c703647739fd9bea935ee40b6122e922bba85174
Rick Lamers <ricklamers@gmail.com>
2022-07-04 13:50:48 +0000

    Init

---
+++ .orchest/environments/085b9595-234b-4e55-9637-5217b5e5e5b0/setup_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install any dependencies you have in this shell script,
+# see https://docs.orchest.io/en/latest/fundamentals/environments.html#install-packages
+
+pip install scikit-learn pandas matplotlib
